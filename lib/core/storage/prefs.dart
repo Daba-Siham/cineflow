04549df7e94170f3202017569a10a1bf56c923a1
Daba-Siham <sihamdaba@ump.ac.ma>
2025-12-27 21:47:50 +0100

    chi 7aja

---
+++ lib/core/storage/prefs.dart
@@ -0,0 +1,16 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppPrefs {
+  // Pour récupérer une instance facilement
+  static Future<SharedPreferences> get instance async =>
+      await SharedPreferences.getInstance();
+
+  // Clés utilisées
+  static const String keyIsDarkMode = 'isDarkMode';
+
+  static const String keyIsLoggedIn = 'isLoggedIn';
+  static const String keyUserId = 'userId
+  static const String keyUsername = 'username';
+  static const String keyEmail = 'email';
+  static const String keyImgProfile = 'imgProfile';
+}
